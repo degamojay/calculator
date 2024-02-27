7a4a87d47fcce2906c092f9e0dababd3152bb0af
Jay Emman <jaydegamo22@gmail.com>
2024-02-27 13:59:40 +0800

    2nd commit

--- lib/calculator/data/repository/calculator_repository.dart
+++ lib/calculator/data/repository/calculator_repository.dart
@@ -1,26 +1,17 @@
 class CalculatorRepository {
-
   int addition(int num1, int num2) {
-
     return num1 + num2;
-
   }
 
-  double subtraction(double num1, double num2){
-
+  double subtraction(double num1, double num2) {
     return num1 - num2;
-
   }
 
   double division(double num1, double num2) {
-
     return num1 == 0 || num2 == 0 ? throw Exception() : num1 / num2;
-
   }
 
-  double multiplication(double num1, double num2){
-
+  double multiplication(double num1, double num2) {
     return num1 * num2;
   }
-
 }
